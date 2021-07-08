dcb4f2f0ad9d3f8d205446c4d96d3cd53788504a
avin-kumar-sharma <avin.kumar.sharma@gmail.com>
2021-07-08 17:20:02 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Login from "./Components/Login";
 
 
 function App() {
   return (
     <div className="App">
       Hello Doctor!!!
+      <Login/>
     </div>
   );
 }
---
+++ src/Components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Grid,Paper} from '@material-ui/core';
+const Login=()=>{
+return(
+    <>
+    <Grid>
+        <Paper elevation={10}></Paper>
+    </Grid>
+    </>
+)
+}
+export default Login;
