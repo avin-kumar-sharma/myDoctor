0c0b362b21b4c604c42fcc12c2a46b6658b06120
Sunil <chandrasunil93@gmail.com>
2021-07-13 21:29:41 +0530

    Revert "doctor details and slot booking"
    This reverts commit ecb873d2a83e68eb876f4e2f47ba26a40cfefc3d.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Dashboard from "./dashboard";
-import DoctorsDetailAndBooking from './DoctorsDetailAndBooking/DoctorsDetailAndBooking';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={DoctorsDetailAndBooking} />
+        <Route path="/" exact component={Dashboard} />
       </Switch>
     </Router>
   );
