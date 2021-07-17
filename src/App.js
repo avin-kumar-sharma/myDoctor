20326f3b1ed78fd9bbe8ff759b8b014a821fea41
avin-kumar-sharma <avin.kumar.sharma@gmail.com>
2021-07-17 09:49:54 +0530

    Merged App.js post conflict resolution

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { createMuiTheme,ThemeProvider } from "@material-ui/core";
+import SignInOutContainer from "./Containers/Index";
 import Dashboard from "./dashboard";
 import DoctorsDetailAndBooking from "./DoctorsDetailAndBooking/DoctorsDetailAndBooking";
+
+const theme = createMuiTheme({
+  palette:{
+    primary:{
+      main:'rgb(102, 204, 255)',      
+    },
+  }
+})
 
 function App() {
   return (
     <Router>
       <Switch>
+        <ThemeProvider theme={theme}>
+          <SignInOutContainer/>
+        </ThemeProvider>
         <Route path="/" exact component={Dashboard} />
         <Route path="/doctorDetailsAndBooking" exact component={DoctorsDetailAndBooking} />
       </Switch>
