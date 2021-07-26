cb1161515e1c1b2945d0fe72a9f2cfbc7a706d29
Avinash Kumar <avin.kumar.sharma@gmail.com>
2021-07-26 15:18:07 +0530

    Fixed login issue and improved appointment component to load details from session.

--- src/DoctorsDetailAndBooking/components/SlotBooking/SlotBooking.jsx
+++ src/DoctorsDetailAndBooking/components/SlotBooking/SlotBooking.jsx
@@ -8,113 +8,127 @@ import Typography from "@material-ui/core/Typography";
 import { useStyles } from "./styles";
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import {setAppointmentData} from "../../../state/appointment/slice.js"
+import { setAppointmentData } from "../../../state/appointment/slice.js";
 
 function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-  
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`scrollable-auto-tabpanel-${index}`}
-      aria-labelledby={`scrollable-auto-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
+	const { children, value, index, ...other } = props;
+
+	return (
+		<div
+			role="tabpanel"
+			hidden={value !== index}
+			id={`scrollable-auto-tabpanel-${index}`}
+			aria-labelledby={`scrollable-auto-tab-${index}`}
+			{...other}
+		>
+			{value === index && (
+				<Box p={3}>
+					<Typography>{children}</Typography>
+				</Box>
+			)}
+		</div>
+	);
 }
 
 function a11yProps(index) {
-  return {
-    id: `scrollable-auto-tab-${index}`,
-    "aria-controls": `scrollable-auto-tabpanel-${index}`,
-  };
+	return {
+		id: `scrollable-auto-tab-${index}`,
+		"aria-controls": `scrollable-auto-tabpanel-${index}`,
+	};
 }
 
 const SlotBooking = (props) => {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-  
-  const dispatch = useDispatch();
-  const history = useHistory();
+	const classes = useStyles();
+	const [value, setValue] = React.useState(0);
 
-  let { availableSlots } = props.data;
-  const doctorId = props.data._id;
+	const dispatch = useDispatch();
+	const history = useHistory();
 
-  dispatch(setAppointmentData(null));
+	let { availableSlots } = props.data;
+	const doctorId = props.data._id;
+	const consultationFee = props.data.consultationFee;
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+	dispatch(setAppointmentData(null));
 
-  function selectSlot(slotId, timeId) {
-    const idxSlot = availableSlots.findIndex((s) => s._id === slotId);
-    const idxTime = availableSlots[idxSlot].time.findIndex((t) => t._id === timeId);
-    const date = new Date(availableSlots[idxSlot].date);
-    const startTime = availableSlots[idxSlot].time[idxTime].startTime;
-    const endTime = availableSlots[idxSlot].time[idxTime].endTime;
-    dispatch(setAppointmentData({
-      date: formatDate(date),
-      startTime: formatTime(startTime),
-      endTime: formatTime(endTime),
-      doctorId,
-    }));
-    history.push(`/self-appointment`);
-  }
+	const handleChange = (event, newValue) => {
+		setValue(newValue);
+	};
 
-  function formatTime(time){
-    const parts = time.split(":");
-    const hours = parseInt(parts[0]);
-    const mins = parseInt(parts[1]);
-    return `${String(hours).padStart(2, "0")}${String(mins).padStart(2, "0")}`;
-  }
+	function selectSlot(slotId, timeId) {
+		const idxSlot = availableSlots.findIndex((s) => s._id === slotId);
+		const idxTime = availableSlots[idxSlot].time.findIndex(
+			(t) => t._id === timeId
+		);
+		const date = new Date(availableSlots[idxSlot].date);
+		const startTime = availableSlots[idxSlot].time[idxTime].startTime;
+		const endTime = availableSlots[idxSlot].time[idxTime].endTime;
 
-  function formatDate(dateObj) {
-    const year = dateObj.getFullYear();
-    const day = dateObj.getDate();
-    const month = dateObj.getMonth() + 1;
-    return `${year}-${String(month).padStart(2, "0")}-${String(day).padStart(2, "0")}`;
-  }
+		dispatch(
+			setAppointmentData({
+				date: formatDate(date),
+				startTime: formatTime(startTime),
+				endTime: formatTime(endTime),
+				doctorId,
+				consultationFee,
+			})
+		);
+		history.push(`/self-appointment`);
+	}
 
-  return (
-    <React.Fragment>
-      <AppBar position="static" color="default">
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="scrollable"
-          scrollButtons="auto"
-        >
-          {availableSlots.map((slotInfo, index) => (
-            <Tab label={slotInfo.date} {...a11yProps(index)} />
-          ))}
-        </Tabs>
-      </AppBar>
-      {availableSlots.map((slotInfo, index) => (
-        <TabPanel value={value} index={index}>
-          <div className={classes.root}>
-            {slotInfo.time.map((slot) => (
-              <Chip
-                variant="outlined"
-                label={slot.startTime + " - " + slot.endTime}
-                clickable
-                onClick = {() => {selectSlot(slotInfo._id, slot._id)}}
-                color="primary"
-              />
-            ))}
-          </div>
-        </TabPanel>
-      ))}
-    </React.Fragment>
-  );
+	function formatTime(time) {
+		const parts = time.split(":");
+		const hours = parseInt(parts[0]);
+		const mins = parseInt(parts[1]);
+		return `${String(hours).padStart(2, "0")}${String(mins).padStart(
+			2,
+			"0"
+		)}`;
+	}
+
+	function formatDate(dateObj) {
+		const year = dateObj.getFullYear();
+		const day = dateObj.getDate();
+		const month = dateObj.getMonth() + 1;
+		return `${year}-${String(month).padStart(2, "0")}-${String(
+			day
+		).padStart(2, "0")}`;
+	}
+
+	return (
+		<React.Fragment>
+			<AppBar position="static" color="default">
+				<Tabs
+					value={value}
+					onChange={handleChange}
+					indicatorColor="primary"
+					textColor="primary"
+					variant="scrollable"
+					scrollButtons="auto"
+				>
+					{availableSlots.map((slotInfo, index) => (
+						<Tab label={slotInfo.date} {...a11yProps(index)} />
+					))}
+				</Tabs>
+			</AppBar>
+			{availableSlots.map((slotInfo, index) => (
+				<TabPanel value={value} index={index}>
+					<div className={classes.root}>
+						{slotInfo.time.map((slot) => (
+							<Chip
+								variant="outlined"
+								label={slot.startTime + " - " + slot.endTime}
+								clickable
+								onClick={() => {
+									selectSlot(slotInfo._id, slot._id);
+								}}
+								color="primary"
+							/>
+						))}
+					</div>
+				</TabPanel>
+			))}
+		</React.Fragment>
+	);
 };
 
 export default SlotBooking;
--- src/state/user/slice.js
+++ src/state/user/slice.js
@@ -1,125 +1,114 @@
-import { createSlice } from '@reduxjs/toolkit';
-import {
-  getProfileAPI,
-  loginAPI,
-  signupAPI,
-} from './api';
-
+import { createSlice } from "@reduxjs/toolkit";
+import { getProfileAPI, loginAPI, signupAPI } from "./api";
 
 const initialState = {
-  token: null,
-  profile: null,
-  loading: false,
-  error: null,
+	token: null,
+	profile: null,
+	loading: false,
+	error: null,
 };
 
 const slice = createSlice({
-  name: 'user',
-  initialState,
-  reducers: {
-    showLoading(state) {
-      state.loading = true;
-      state.profile = null;
-      state.error = null;
-    },
-    hideLoading(state) {
-      state.loading = false;
-      state.profile = null;
-      state.error = null;
-    },
-    logout(state) {
-      state.token = null;
-      state.profile = null;
-      state.loading = false;
-      state.error = null;
-      localStorage.removeItem('auth-token');
-      localStorage.removeItem('user-id');
-    },
-    loginSuccess(state, action) {
-      state.token = action.payload.data.token;
-      state.profile = action.payload.data.user;   
-      localStorage.setItem('user-id', state.profile._id);
-      state.loading = false;
-      state.error = null;
-    },
-    loginFailed(state, action) {
-      state.profile = null;
-      state.error = action.payload ?? "login error";
-      state.loading = false;
-    },
-    loadProfileSuccess(state, action) {
-      state.profile = action.payload;
-      state.token = localStorage.getItem('auth-token');
-      state.loading = false;
-      state.error = null;
-    },
-    loadProfileFailed(state, action) {
-      state.profile = null;
-      state.error = action.payload ?? "profile error";
-      state.loading = false;
-    },
-    signupSuccess(state, action) {
-      state.signupSuccessMessage = action.payload;
-      state.loading = false;
-      state.error = null;
-    },
-    signupFailed(state, action) {
-      state.profile = null;
-      state.error = action.payload ? action.payload.data ?? "signup error" : "signup error";
-      state.loading = false;
-    },
-  },
+	name: "user",
+	initialState,
+	reducers: {
+		showLoading(state) {
+			state.loading = true;
+			state.profile = null;
+			state.error = null;
+		},
+		hideLoading(state) {
+			state.loading = false;
+			state.profile = null;
+			state.error = null;
+		},
+		logout(state) {
+			state.token = null;
+			state.profile = null;
+			state.loading = false;
+			state.error = null;
+			localStorage.removeItem("auth-token");
+			localStorage.removeItem("user-id");
+		},
+		loginSuccess(state, action) {
+			state.token = action.payload.data.token;
+			state.profile = action.payload.data.user;
+			localStorage.setItem("user-id", state.profile._id);
+			state.loading = false;
+			state.error = null;
+		},
+		loginFailed(state, action) {
+			state.profile = null;
+			state.error = action.payload ?? "login error";
+			state.loading = false;
+		},
+		loadProfileSuccess(state, action) {
+			state.profile = action.payload;
+			state.loading = false;
+			state.error = null;
+		},
+		loadProfileFailed(state, action) {
+			state.profile = null;
+			state.error = action.payload ?? "profile error";
+			state.loading = false;
+		},
+		signupSuccess(state, action) {
+			state.signupSuccessMessage = action.payload;
+			state.loading = false;
+			state.error = null;
+		},
+		signupFailed(state, action) {
+			state.profile = null;
+			state.error = action.payload
+				? action.payload.data ?? "signup error"
+				: "signup error";
+			state.loading = false;
+		},
+	},
 });
 
 export const {
-  showLoading,
-  hideLoading,
-  logout,
-  loginSuccess,
-  loginFailed,
-  signupSuccess,
-  signupFailed,
-  loadProfileSuccess,
-  loadProfileFailed,
+	showLoading,
+	hideLoading,
+	logout,
+	loginSuccess,
+	loginFailed,
+	signupSuccess,
+	signupFailed,
+	loadProfileSuccess,
+	loadProfileFailed,
 } = slice.actions;
 
 export default slice.reducer;
 
-export const signup = (
-  payload,
-  callback
-) => async (dispatch) => {
-  dispatch(showLoading());
-  try {
-    const res = await signupAPI(payload);
-    dispatch(signupSuccess(res.data));
-    if (callback !== undefined) callback();
-  } catch (error) {
-    dispatch(signupFailed(error));
-  }
+export const signup = (payload, callback) => async (dispatch) => {
+	dispatch(showLoading());
+	try {
+		const res = await signupAPI(payload);
+		dispatch(signupSuccess(res.data));
+		if (callback !== undefined) callback();
+	} catch (error) {
+		dispatch(signupFailed(error));
+	}
 };
-
 
 export const loadprofile = (payload) => async (dispatch) => {
-  dispatch(showLoading());
-  try {
-    const res = await getProfileAPI(payload);
-    dispatch(loadProfileSuccess(res.data));
-  } catch (err) {
-    dispatch(loadProfileFailed(err));
-  }
+	dispatch(showLoading());
+	try {
+		const res = await getProfileAPI(payload);
+		dispatch(loadProfileSuccess(res.data));
+	} catch (err) {
+		dispatch(loadProfileFailed(err));
+	}
 };
 
-export const login = (payload) => async (
-  dispatch
-) => {
-  dispatch(showLoading());
-  try {
-    const res = await loginAPI(payload);
-    console.log(res);
-    dispatch(loginSuccess(res.data.data));
-    //loadprofile()(dispatch);
-  } catch (err) {
-    dispatch(loginFailed(err));
-  }
+export const login = (payload) => async (dispatch) => {
+	dispatch(showLoading());
+	try {
+		const res = await loginAPI(payload);
+		dispatch(loginSuccess(res.data));
+		//loadprofile()(dispatch);
+	} catch (err) {
+		dispatch(loginFailed(err));
+	}
 };
