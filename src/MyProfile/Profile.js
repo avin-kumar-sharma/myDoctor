1faa4a580214a814bb431851219a5c46a3190677
Sunil <chandrasunil93@gmail.com>
2021-07-28 22:00:28 +0530

    profile page added

--- src/MyProfile/Profile.js
+++ src/MyProfile/Profile.js
@@ -1,2 +1,87 @@
-import React from 'react';
+import { Grid } from "@material-ui/core";
+import React, { useEffect } from "react";
+import Page from "../layout/Page/page";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import { useStyles } from "./styles";
 
+const Profile = (props) => {
+  const [profileInfo, setProfileInfo] = React.useState({});
+  const classes = useStyles();
+  const clientId = localStorage.getItem("user-id");
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/v1/user/${clientId}`)
+      .then((response) => response.json())
+      .then((data) => setProfileInfo(data.message));
+  }, []);
+
+  if (profileInfo == null) {
+    return (
+      <Page>
+        <div>Loading...</div>
+      </Page>
+    );
+  }
+
+  return (
+    <Page>
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={profileInfo.profileImage}
+            title="Doctor image"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h6">
+              Name :
+            </Typography>
+            <Typography gutterBottom variant="h4" component="h2">
+              {profileInfo.firstName + " " + profileInfo.lastName}
+            </Typography>
+            <Typography gutterBottom variant="h6">
+              E-Mail :
+            </Typography>
+            <Typography gutterBottom variant="h4" component="h2">
+              {profileInfo.email}
+            </Typography>
+            <Typography gutterBottom variant="h6">
+              Gender :
+            </Typography>
+            <Typography gutterBottom variant="h4" component="h2">
+              {profileInfo.gender}
+            </Typography>
+            <Typography gutterBottom variant="h6">
+              Mobile :
+            </Typography>
+            <Typography gutterBottom variant="h4" component="h2">
+              {profileInfo.contactNumber}
+            </Typography>
+            <Typography gutterBottom variant="h6">
+              Alternate Mobile :
+            </Typography>
+            <Typography gutterBottom variant="h4" component="h2">
+              {profileInfo.alternateNumber}
+            </Typography>
+            <Typography gutterBottom variant="h6">
+              Address :
+            </Typography>
+            <Typography gutterBottom variant="h4" component="h2">
+              {profileInfo.address}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Page>
+  );
+};
+
+export default Profile;
