9dbaaa5b2a0c1384d6c9cd64c8e61669d9babb1f
Sunil <chandrasunil93@gmail.com>
2021-07-29 15:54:03 +0530

    Update profileSection.jsx

--- src/layout/components/ProfileSection/profileSection.jsx
+++ src/layout/components/ProfileSection/profileSection.jsx
@@ -49,7 +49,7 @@ export default function ProfileSection() {
           keepMounted
           open={Boolean(anchorEl)}
         >
-          <MenuItem onClick={handleClose}>
+          <MenuItem
             onClick={() => {
               history.push("/profile");
             }}
